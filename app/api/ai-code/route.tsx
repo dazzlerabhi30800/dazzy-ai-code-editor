3d121ae49137d203fe67eb0d276b029285fe8e64
dazzlerabhi30800 <abhisingh30800@gmail.com>
2025-01-18 18:27:16 +0530

    checking if it ain't working

--- app/api/ai-code/route.tsx
+++ app/api/ai-code/route.tsx
@@ -4,7 +4,10 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   try {
     const { prompt } = await req.json();
-    const result = await codeChatSession.sendMessage(prompt);
+    console.log(prompt);
+    const result = await codeChatSession.sendMessage(
+      "you are very good in this world we cannot hope for better",
+    );
     const resp = result.response.text();
     return NextResponse.json({ fileData: resp });
   } catch (err: unknown) {
