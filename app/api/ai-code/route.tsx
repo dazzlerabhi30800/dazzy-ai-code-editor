e9efd0aeb751b8a510621adf23fb9e28e8955bd0
dazzlerabhi30800 <abhisingh30800@gmail.com>
2025-01-18 20:16:20 +0530

    let's see if it's working

--- app/api/ai-code/route.tsx
+++ app/api/ai-code/route.tsx
@@ -4,6 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   try {
     const { prompt } = await req.json();
+    if (!prompt) {
+      return NextResponse.json({
+        error: "Prompt not found",
+      });
+    }
     const result = await codeChatSession.sendMessage(prompt);
     const resp = result.response.text();
     return NextResponse.json({ fileData: resp });
