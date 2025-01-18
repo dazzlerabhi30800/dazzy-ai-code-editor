340b35ce7c82e7df4a19f8e6e901a316e1499c44
dazzlerabhi30800 <abhisingh30800@gmail.com>
2025-01-18 18:59:35 +0530

    added new type

--- app/api/ai-code/route.tsx
+++ app/api/ai-code/route.tsx
@@ -4,7 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   try {
     const { prompt } = await req.json();
-    const result = await codeChatSession.sendMessage(prompt.toString());
+    console.log(prompt);
+    // const result = await codeChatSession.sendMessage(prompt.toString());
+    const result = await codeChatSession.sendMessage("very well in the world we are talking about in the world");
     const resp = result.response.text();
     return NextResponse.json({ fileData: resp });
   } catch (err: unknown) {
